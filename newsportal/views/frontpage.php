8e13df548d265ae808d589d56dd2c0ffca1f8982
Majd Ewawi <majd1999ziad@gmail.com>
2020-04-15 22:34:04 +0300

    fetch news from database integrate data within the page

--- newsportal/views/frontpage.php
+++ newsportal/views/frontpage.php
@@ -103,89 +103,128 @@
 					</div>
 				</div>
 			</div>
+			<?php
+        		$query = "SELECT * FROM news ORDER BY dateposted DESC LIMIT 9 ";
+				$result = mysqli_query($connection , $query);  
+        		$row = mysqli_fetch_array($result);
+    		?>
 			<div class="d-flex border border-danger flex-column">
 				<div class="d-flex">
 					<div class="flex-column border-bottom border-danger md-2">
 						<div class="p-2 d-flex border-bottom border-light">
-							<p class="p-2">
-								The world's drastic measures in response to
-								coronavirus
+							<p class="p-2" style="width: 15rem; height: 5rem;">
+								<?php echo $row['title'] ; ?> 
+								<a href="">...</a>
 							</p>
 							<img
-								style="width: 5rem;"
-								src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+								class="ml-auto"
+								style="width: 5rem; height: 5rem;"
+								src="./../assets/images/<?php echo $row[4] ; ?>"
 								alt="Card image cap"
 							/>
 						</div>
+						<?php
+						    $row = mysqli_fetch_array($result);
+    					?>
 						<div class="p-2 d-flex border-bottom border-light">
-							<p class="p-2">
-								The world's drastic measures in response to
-								coronavirus
+							<p class="p-2" style="width: 15rem; height: 5rem;">
+								<?php echo $row['title'] ; ?>
+								<a href="">...</a>
 							</p>
 							<img
-								style="width: 5rem;"
-								src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+								class="ml-auto"
+								style="width: 5rem; height: 5rem;"
+								src="./../assets/images/<?php echo $row[4] ; ?>"
 								alt="Card image cap"
 							/>
 						</div>
+						<?php
+						    $row = mysqli_fetch_array($result);
+    					?>
 						<div class="p-2 d-flex border-bottom border-light">
-							<p class="p-2">
-								The world's drastic measures in response to
-								coronavirus
+							<p class="p-2" style="width: 15rem; height: 5rem;">
+								<?php echo $row['title'] ; ?>
+								<a href="">...</a>
 							</p>
 							<img
-								style="width: 5rem;"
-								src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+								class="ml-auto"
+								style="width: 5rem; height: 5rem;"
+								src="./../assets/images/<?php echo $row[4] ; ?>"
 								alt="Card image cap"
 							/>
 						</div>
+						<?php
+						    $row = mysqli_fetch_array($result);
+    					?>
 						<div class="p-2 d-flex border-bottom border-light">
-							<p class="p-2">
-								The world's drastic measures in response to
-								coronavirus
+							<p class="p-2" style="width: 15rem;">
+								<?php echo $row['title'] ; ?>
+								<a href="">...</a>
 							</p>
 							<img
+								class="ml-auto"
 								style="width: 5rem;"
-								src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+								src="./../assets/images/<?php echo $row[4] ; ?>"
 								alt="Card image cap"
 							/>
 						</div>
 					</div>
+					<?php
+						$row = mysqli_fetch_array($result);
+    				?>
 					<div class="flex-column p-2 " style="width: 35rem;">
 						<img
 							class="card-img-top"
-							src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+							src="./../assets/images/<?php echo $row[4] ; ?>"
 							alt="Card image cap"
 						/>
-						<p class="card-text p-2">
-							The world's drastic measures in response to
-							coronavirus The world's drastic
-						</p>
+					<p class="card-text p-2">
+						<?php echo $row['title'] ; ?>
+						<a href="">...</a>
+					</p>
 					</div>
+
 				</div>
 				<div class="d-flex flex-row m-auto">
+					<?php
+						$row = mysqli_fetch_array($result);
+    				?>
+
 					<img
 						style="width: 14rem;"
 						class="p-2"
-						src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+						src="./../assets/images/<?php echo $row[4] ; ?>"
 						alt="Card image cap"
 					/>
+			
+					<?php
+						$row = mysqli_fetch_array($result);
+    				?>
+
 					<img
 						style="width: 14rem;"
 						class="p-2"
-						src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+						src="./../assets/images/<?php echo $row[4] ; ?>"
 						alt="Card image cap"
 					/>
+					<?php
+						$row = mysqli_fetch_array($result);
+    				?>
+
 					<img
 						style="width: 14rem;"
 						class="p-2"
-						src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+						src="./../assets/images/<?php echo $row[4] ; ?>"
 						alt="Card image cap"
 					/>
+					<?php
+						$row = mysqli_fetch_array($result);
+    				?>
+
 					<img
 						style="width: 14rem;"
 						class="p-2"
-						src="./../assets/images/200401135051-hong-kong-farm-large-tease.jpeg"
+						src="./../assets/images/<?php echo $row[4] ; ?>"
 						alt="Card image cap"
 					/>
 				</div>
