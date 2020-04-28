6b90017310a648f9af6b0a82a397f6e212dfb5cb
Majd Ewawi <majd1999ziad@gmail.com>
2020-04-29 02:46:11 +0300

    escape special characters in add news page

--- adminpanel/usersfunctionality/addnews.php
+++ adminpanel/usersfunctionality/addnews.php
@@ -6,19 +6,22 @@ $id = $_SESSION['id'];
 
 if (isset($_POST['submit'])) {
 
-   
+
     $title = $_POST['title'];
     $body = $_POST['body'];
     $isUrgent = $_POST['isUrgent'];
     $isGlobal = $_POST['isGlobal'];
     $category = $_POST['category'];
 
+    $newtitle = mysqli_escape_string($connection, $title);
+    $newbody = mysqli_escape_string($connection, $body);
+
     if (isset($_FILES['news'])) {
         $image = $_FILES['news']['name'];
-     
+
 
         $query = "INSERT INTO news (title, body, category , image , isUrgent , isGlobal ,writerId) 
-                    VALUES ('$title', '$body','$category', '$image', $isUrgent , $isGlobal , $id )";
+                    VALUES ('$newtitle', '$newbody','$category', '$image', $isUrgent , $isGlobal , $id )";
 
         $result = mysqli_query($connection, $query);
 
@@ -26,7 +29,7 @@ if (isset($_POST['submit'])) {
             move_uploaded_file($_FILES['news']['tmp_name'], "./../../images/news/$image");
             header("location:./../views/usersviews/allnews.php");
         } else {
-           header("location:./../views/usersviews/addnews.php?error=1");
+            header("location:./../views/usersviews/addnews.php?error=1");
         }
     }
 } else {
